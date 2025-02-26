9a1effa6f8bb9e624c3661995faf2d168845daa3
YuriyNotPickle <yurriyklimenko@gmail.com>
2025-02-26 18:44:35 +0300

    Этап 2

--- gendiff.js
+++ gendiff.js
@@ -8,6 +8,8 @@ program
 	.name('gendiff')
 	.description('Compares two configuration files and shows a difference.')
 	.version('1.0.0', '-V, --version', 'output the version number')
+	.option('-f, --format <type>', 'output format', 'stylish') // Опция для выбора формата
+	.arguments('<filepath1> <filepath2>') // Аргументы для путей к файлам
 	.helpOption('-h, --help', 'output usage information');
 
 program.parse(process.argv);
